676ed7e0a8181f0a180ca9beecd9dc67b69fe0f9
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-06-03 16:45:12 -0400

    playground 2

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//     }
+//   );
+
+// module.exports = sequelize;
+
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize(process.env.POSTGRESURI)
+module.exports = sequelize;
