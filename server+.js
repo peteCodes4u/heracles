ebbe3483d2d71ad62c200290e077e96b7ada0f6f
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-06-03 21:21:15 -0400

    done

--- server+.js
+++ server+.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.SESSIONSECRET,
@@ -29,8 +28,6 @@ app.use(express.static('public'));
 
 // Add express-session and store as Express.js middleware
 app.use(session(sess));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -39,9 +36,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // get route
-// app.get('/', async(req,res) =>{
-//   await res.sendFile(path.join(__dirname, './public/index.html'))
-// });
+app.get('/', async(req,res) =>{
+  await res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`😊 App listening on port ${PORT}`));
